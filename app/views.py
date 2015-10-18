bc4972e748050d856f639f408c477d7d1c111dcd
joe song <ijoosong@gmail.com>
2015-10-17 22:04:33 -0400

    yup

--- app/views.py
+++ app/views.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index/')
 def index():
     user = {'name': 'Zach'}
-    return render_template('index.html', title='Home', user=user)
+    tweets = [
+        {
+            'username': 'ijoosong',
+            'message': 'Lol is this thing working?'
+        },
+        {
+            'username': 'inpursuitofswag',
+            'message': 'lawlz you aint know crap'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, tweets=tweets)
