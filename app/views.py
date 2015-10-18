4fe21eb4ebdfd5fe9ee7d3a06736f79d82a64b26
joe song <ijoosong@gmail.com>
2015-10-17 21:50:54 -0400

    added some templating

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return "Hi zach. you're dumb. oh well. :D"
+    user = {'name': 'Zach'}
+    return render_template('index.html', title='Home', user=user)
